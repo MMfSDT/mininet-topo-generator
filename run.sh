cb98ec208a19f21376bdb06dab43b0be3e78512e
Charles Josh <cjoshua.alba@gmail.com>
2018-04-05 22:10:54 +0800

    Fix run script and topogen args

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 #   run.sh
 #       Bootstrap a static topology.
 #       Virtually follows topogen.py's syntax:
-#           ./run.sh 
+#           sudo ./run.sh 
 #               [--test path_to_test {none}] 
 #               [--post path_to_post_process_script {none}]
 #               [--router router_behavior {static}]
@@ -13,23 +13,24 @@
 #               [--proto tcp|mptcp {mptcp}]
 #               [--pmanager fullmesh|ndiffports {fullmesh}]
 #               [--diffports num_diff_ports {1}]
+#               [--juggler]
 #               [--payloadsize query|long|short {short}]
 #               [--runcount num_counts {10}]
 #       Make sure to set env.sh first before proceeding.
 ############################################################################################
+
+# *** Check arguments before script execution ***
+# Quit the program if it wasn't executed as root.
+if [[ $EUID -ne 0 ]]; then
+    echo "[Error] run.sh must be executed as root. Quitting."
+    exit 1
+fi
 
 # Export all the prerequisite environmental variables for this process.
 set -o allexport
 source env.sh
 set +o allexport
 
-# Clean the mess Mininet makes from a failed exit silently.
-sudo mn -c &> /dev/null
-
-# Clean old .pcap traces in case of errors.
-rm s*.pcap
-
-# Find the test script, if indicated.
 # Store arguments first as we're mutating it.
 args=("$@")
 
@@ -61,23 +62,13 @@ if [[ -z "$diffports" ]]; then diffports="1"; fi
 if [[ -z "$payloadsize" ]]; then payloadsize="short"; fi
 if [[ -z "$runcount" ]]; then runcount="10"; fi
 
-# It is necessary to create a source file for Mininet to parse.
-#   This automatically generated file is at "./kickstart_python.test"
-if [[ -z "$test" ]]; then
-    # If "--test" wasn't given as an argument, remove existing source file.
-    echo "No test to execute."
-    rm kickstart_python.test &> /dev/null
-elif [[ ! -f "$test" ]]; then
-    # If "--test" file does not exist, remove existing source file.
-    echo "File \"$test\" does not exist. Skipping."
-    rm kickstart_python.test &> /dev/null
-else 
-    # If "--test" file does exist, write the source file.
-    echo "py execfile(\"$test\")" > kickstart_python.test
-    echo "Running test: \"$test\""
+# Quit the script if it is run with a post-processing script (--post) without pcap-logging enabled (--pcap).
+if [[ ! -z "$post" ]] && [[ -z "$pcap" ]]; then
+    echo "run.sh: can't run post-processing script without --pcap"
+    exit 1
 fi
 
-# Set the TOPO_JSON and TOPO_TABLEGEN paths accordingly.
+# Set the TOPO_JSON and TOPO_TABLEGEN paths accordingly, quit if incorrect value.
 if [ "$router" == "static" ]; then
     TOPO_JSON_PATH=$TOPO_JSON_SIMPLE_PATH
     TOPO_TABLEGEN_PATH=$TOPO_TABLEGEN_SIMPLE_PATH
@@ -92,10 +83,26 @@ else
     exit 1
 fi
 
-# Quit the script if it is run with a post-processing script (--post) without pcap-logging enabled (--pcap).
-if [[ ! -z "$post" ]] && [[ -z "$pcap" ]]; then
-    echo "run.sh: can't run post-processing script without --pcap"
-    exit 1
+# Clean the mess Mininet makes from a failed exit silently.
+mn -c &> /dev/null
+
+# Clean old .pcap traces in case of errors.
+rm s*.pcap
+
+# It is necessary to create a source file for Mininet to parse.
+#   This automatically generated file is at "./kickstart_python.test"
+if [[ -z "$test" ]]; then
+    # If "--test" wasn't given as an argument, remove existing source file.
+    echo "No test to execute."
+    rm kickstart_python.test &> /dev/null
+elif [[ ! -f "$test" ]]; then
+    # If "--test" file does not exist, remove existing source file.
+    echo "File \"$test\" does not exist. Skipping."
+    rm kickstart_python.test &> /dev/null
+else 
+    # If "--test" file does exist, write the source file.
+    echo "py execfile(\"$test\")" > kickstart_python.test
+    echo "Running test: \"$test\""
 fi
 
 # Create the argument file for the test file in JSON.
@@ -116,18 +123,25 @@ echo "\"payloadsize\": \"$payloadsize\"," >> ../network-tests/logs/args.txt
 echo "\"runcount\": \"$runcount\"" >> ../network-tests/logs/args.txt
 echo "}" >> ../network-tests/logs/args.txt
 
-# Set the environmental variables before running the topology generator.
-TOPO_EXEC_PATH=$TOPO_EXEC_PATH
-TOPO_CLI_PATH=$TOPO_CLI_PATH
+# Prepare topogen.py arguments
+TOPOGEN_ARGS=(--exec_path $TOPO_EXEC_PATH --json_path $TOPO_JSON_PATH --cli_path $TOPO_CLI_PATH --tablegen_path $TOPO_TABLEGEN_PATH --K $K)
+if [[ ! -z "$pcap" ]]; then TOPOGEN_ARGS+=(--pcap); fi
+if [[ ! -z "$test" ]]; then TOPOGEN_ARGS+=(--test $test); fi
 
-# Then finally run the generator, passing fully all arguments.
-    sudo ./topogen.py "$@" --exec_path $TOPO_EXEC_PATH --json_path $TOPO_JSON_PATH --cli_path $TOPO_CLI_PATH --tablegen_path $TOPO_TABLEGEN_PATH || exit 1
+# Finally, run topogen.py with the arguments specified above.
+./topogen.py ${TOPOGEN_ARGS[*]}  || exit 1
 
 # Clean the mess again after exiting, silently.
-sudo mn -c &> /dev/null
+mn -c &> /dev/null
+
+# Fix the file ownership of the *.pcap at log files.
+chown $SUDO_USER:$SUDO_USER ../network-tests/logs/args.txt
+chown $SUDO_USER:$SUDO_USER ../network-tests/logs/aggregate.json
+chown $SUDO_USER:$SUDO_USER ../network-tests/logs/mid.json
+chown $SUDO_USER:$SUDO_USER s*.pcap
 
 # Run the postprocessing file, then delete all traces.
 if [[ ! -z "$post" ]]; then
-    sudo -u "$SUDO_USER" ./$post "$@" || exit 1
-    sudo rm s*.pcap
+    sudo -u $SUDO_USER ./$post "$@" || exit 1
+    rm s*.pcap
 fi
--- topogen.py
+++ topogen.py
@@ -5,20 +5,13 @@
 #       Generates a scalable fat-tree topology.
 #       Follows this syntax:
 #           ./topogen.py
-#               [--test path_to_test {none}] 
-#               [--post path_to_post_process_script {none}]
-#               [--router router_behavior {static}]
+#               [--test path_to_test {none}]
 #               [--pcap]
 #               [--K K {4}]
-#               [--proto tcp|mptcp {mptcp}]
-#               [--pmanager fullmesh|ndiffports {fullmesh}]
-#               [--diffports num_diff_ports {1}]
-#               [--payloadsize query|long|short {short}]
-#               [--runcount num_counts {10}]
-# 				[--exec_path exec_path {../behavioral-model/targets/simple_router/simple_router}] 
-# 				[--json_path json_path {./router/simple_router.json}] 
-# 				[--cli_path cli_path {../behavioral-model/tools/runtime_CLI.py}] 
-# 				[--tablegen_path tablegen_path {./router/tablegen_simple.py}] 
+# 				[--exec_path exec_path {../behavioral-model/targets/simple_router/simple_router}]
+# 				[--json_path json_path {./router/simple_router.json}]
+# 				[--cli_path cli_path {../behavioral-model/tools/runtime_CLI.py}]
+# 				[--tablegen_path tablegen_path {./router/tablegen_simple.py}]
 #       Make sure to set env.sh first before proceeding.
 ############################################################################################
 
@@ -40,16 +33,8 @@ from router.p4_mininet import P4Switch, P4Host
 
 parser = argparse.ArgumentParser(description='Generates a scalable Fat-tree topology.')
 parser.add_argument('--test', default=None, type=str, metavar='path_to_test', help='specify a test to run. defaults to None.')
-parser.add_argument('--post', default=None, type=str, metavar='path_to_post_process_script', help='specify a post-processing script to run. defaults to None.')
-parser.add_argument('--router', default='static', type=str, metavar='router_behavior', help='configure switch behavior between static, ecmp, and ps. defaults to static.')
 parser.add_argument('--pcap', action='store_true', help='dumps pcap files')
 parser.add_argument('--K', default='4', type=int, metavar='num_ports', help='number of ports per switch. defaults to 4.')
-parser.add_argument('--proto', default='mptcp', type=str, metavar='tcp|mptcp', help='configure host protocol between tcp and mptcp. defaults to mptcp.')
-parser.add_argument('--pmanager', default='fullmesh', type=str, metavar='fullmesh|ndiffports', help='specify a MPTCP path manager. defaults to fullmesh.')
-parser.add_argument('--diffports', default=1, type=int, metavar='num_diff_ports', help='if --pmanager is set to ndiffports, set diffports here. defaults to 1.')
-parser.add_argument('--juggler', action='store_true', help='specify if running Juggler or not. defaults to false.')
-parser.add_argument('--payloadsize', default='short', type=str, metavar='query|long|short', help='specify flow size. defaults to short.')
-parser.add_argument('--runcount', default=10, type=int, metavar='num_counts', help='specify how many tests should be done per pair. defaults to 10.')
 parser.add_argument('--exec_path', default='../behavioral-model/targets/simple_router/simple_router', type=str, help='provide the path to the simple_router executable')
 parser.add_argument('--json_path', default='./router/simple_router.json', type=str, help='provide the path to the behavioral json')
 parser.add_argument('--cli_path', default='../behavioral-model/tools/runtime_CLI.py', type=str, help='provide the path to the runtime CLI')
@@ -57,16 +42,16 @@ parser.add_argument('--tablegen_path', default='./router/tablegen_simple.py', ty
 
 args = parser.parse_args()
 
-exec_path = args.exec_path;
-json_path = args.json_path;
-cli_path = args.cli_path;
-tablegen_path = args.tablegen_path;
+exec_path = args.exec_path
+json_path = args.json_path
+cli_path = args.cli_path
+tablegen_path = args.tablegen_path
 
 # Code proper.
 
 if '__main__' == __name__:
-	setLogLevel('info')
-	net = Mininet(controller=None)
+    setLogLevel('info')
+    net = Mininet(controller=None)
     #key = "net.mptcp.mptcp_enabled"
     #value = 1
     #p = Popen("sysctl -w %s=%s" % (key, value),
@@ -74,159 +59,159 @@ if '__main__' == __name__:
     #stdout, stderr = p.communicate()
     #print "stdout=", stdout, "stderr=", stderr
 
-	K = args.K 									# Moved from argv[1] to args.K
-	print "Generating topology for K =", K
+    K = args.K 									# Moved from argv[1] to args.K
+    print "Generating topology for K =", K
 
-	print "Naming convention"
-	print "Host:               h<pod><i><j>"
-	print "Edge switch:        se<pod><i>"
-	print "Aggregate switch:   sa<pod><i>"
-	print "Core switch:        sc<i><j>"
-	
-	host_ip = [[[
-	'10.%d.%d.%d'%(pod,i,j+2)
-	for j in range(K/2)]
-	for i in range(K/2)]
-	for pod in range(K)]
-	
-	host = [[[
-	net.addHost('h%d%d%d'%(pod,i,j),
-		cls=P4Host,
-		ip=host_ip[pod][i][j])
+    print "Naming convention"
+    print "Host:               h<pod><i><j>"
+    print "Edge switch:        se<pod><i>"
+    print "Aggregate switch:   sa<pod><i>"
+    print "Core switch:        sc<i><j>"
+
+    host_ip = [[[
+        '10.%d.%d.%d'%(pod,i,j+2)
+        for j in range(K/2)]
+        for i in range(K/2)]
+        for pod in range(K)]
+
+    host = [[[
+    net.addHost('h%d%d%d'%(pod,i,j),
+        cls=P4Host,
+        ip=host_ip[pod][i][j])
     for j in range(K/2)]
     for i in range(K/2)]
-	for pod in range(K)]
-	
-	
-	
-	port_offset = 10000
-	
-	edge_port = [[
-	pod*K/2+i + port_offset
-	for i in range(K/2)]
-	for pod in range(K)]
-    
-	agg_port = [[
-	pod*K/2+i + K*K/2 + port_offset
-	for i in range(K/2)]
-	for pod in range(K)]
-    
-	core_port = [[
-	i*K/2+j + K*K + port_offset
-	for j in range(K/2)]
-	for i in range(K/2)]
-	
-	edge = [[
-	net.addSwitch('se%d%d'%(pod,i),
-		cls = P4Switch,
- 		sw_path = exec_path,
-		json_path = json_path,
-		thrift_port = edge_port[pod][i],
-		pcap_dump = args.pcap)
-	for i in range(K/2)]
-	for pod in range(K)]
+    for pod in range(K)]
 
-	agg = [[
-	net.addSwitch('sa%d%d'%(pod,i),
-		cls = P4Switch,
-		sw_path = exec_path,
-		json_path = json_path,
-		thrift_port = agg_port[pod][i],
-		pcap_dump = args.pcap)
-	for i in range(K/2)]
-	for pod in range(K)]
 
-	core = [[
-	net.addSwitch('sc%d%d'%(i,j),
-		cls = P4Switch,
-		sw_path = exec_path,
-		json_path = json_path,
-		thrift_port = core_port[i][j],
-		pcap_dump = args.pcap)
-	for j in range(K/2)]
-	for i in range(K/2)]
-	
-	
-	
-	edge_ip = [[
-	'10.%d.%d.1'%(pod,i)
-	for i in range(K/2)]
-	for pod in range(K)]
-	
-	agg_ip = [[
-	'10.%d.%d.1'%(pod,i)
-	for i in range(K/2,K)]
-	for pod in range(K)]
-	
-	core_ip = [[
-	'10.%d.%d.%d'%(K,i+1,j+1)
-	for j in range(K/2)]
-	for i in range(K/2)]
-	
-	
 
-	linkopt = {'bw': 20}
-    
-	#host to edge
-	for pod in range(K):
-		for i in range(K/2):
-			for j in range(K/2):
-				net.addLink(host[pod][i][j],edge[pod][i],cls=TCLink,**linkopt)
+    port_offset = 10000
 
-	#edge to aggregate
-	for pod in range(K):
-		for i in range(K/2):
-			for j in range(K/2):
-				net.addLink(edge[pod][i],agg[pod][j],cls=TCLink,**linkopt)
+    edge_port = [[
+    pod*K/2+i + port_offset
+    for i in range(K/2)]
+    for pod in range(K)]
 
-	#aggregate to core
-	for pod in range(K):
-		for i in range(K/2):
-			for j in range(K/2):
-				net.addLink(agg[pod][i],core[i][j],cls=TCLink,**linkopt)
-				
-	
-	net.build()
-	net.staticArp()
-	net.start()
-	
-	#configure host forwarding
-	for pod in range(K):
-		for i in range(K/2):
-			for j in range(K/2):
-				host[pod][i][j].setDefaultRoute('dev eth0 via %s'%(edge_ip[pod][i]))
-				# IPv6 messes with the logs. Disable it.
-				host[pod][i][j].cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
-				host[pod][i][j].cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
-				host[pod][i][j].cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
-	
-	#get tablegen to initialize routing tables
-	tablegen = imp.load_source('tablegen',tablegen_path).TableGenerator(
+    agg_port = [[
+    pod*K/2+i + K*K/2 + port_offset
+    for i in range(K/2)]
+    for pod in range(K)]
+
+    core_port = [[
+    i*K/2+j + K*K + port_offset
+    for j in range(K/2)]
+    for i in range(K/2)]
+
+    edge = [[
+    net.addSwitch('se%d%d'%(pod,i),
+        cls = P4Switch,
+         sw_path = exec_path,
+        json_path = json_path,
+        thrift_port = edge_port[pod][i],
+        pcap_dump = args.pcap)
+    for i in range(K/2)]
+    for pod in range(K)]
+
+    agg = [[
+    net.addSwitch('sa%d%d'%(pod,i),
+        cls = P4Switch,
+        sw_path = exec_path,
+        json_path = json_path,
+        thrift_port = agg_port[pod][i],
+        pcap_dump = args.pcap)
+    for i in range(K/2)]
+    for pod in range(K)]
+
+    core = [[
+    net.addSwitch('sc%d%d'%(i,j),
+        cls = P4Switch,
+        sw_path = exec_path,
+        json_path = json_path,
+        thrift_port = core_port[i][j],
+        pcap_dump = args.pcap)
+    for j in range(K/2)]
+    for i in range(K/2)]
+
+
+
+    edge_ip = [[
+    '10.%d.%d.1'%(pod,i)
+    for i in range(K/2)]
+    for pod in range(K)]
+
+    agg_ip = [[
+    '10.%d.%d.1'%(pod,i)
+    for i in range(K/2,K)]
+    for pod in range(K)]
+
+    core_ip = [[
+    '10.%d.%d.%d'%(K,i+1,j+1)
+    for j in range(K/2)]
+    for i in range(K/2)]
+
+
+
+    linkopt = {'bw': 20}
+
+    #host to edge
+    for pod in range(K):
+        for i in range(K/2):
+            for j in range(K/2):
+                net.addLink(host[pod][i][j],edge[pod][i],cls=TCLink,**linkopt)
+
+    #edge to aggregate
+    for pod in range(K):
+        for i in range(K/2):
+            for j in range(K/2):
+                net.addLink(edge[pod][i],agg[pod][j],cls=TCLink,**linkopt)
+
+    #aggregate to core
+    for pod in range(K):
+        for i in range(K/2):
+            for j in range(K/2):
+                net.addLink(agg[pod][i],core[i][j],cls=TCLink,**linkopt)
+
+
+    net.build()
+    net.staticArp()
+    net.start()
+
+    #configure host forwarding
+    for pod in range(K):
+        for i in range(K/2):
+            for j in range(K/2):
+                host[pod][i][j].setDefaultRoute('dev eth0 via %s'%(edge_ip[pod][i]))
+                # IPv6 messes with the logs. Disable it.
+                host[pod][i][j].cmd("sysctl -w net.ipv6.conf.all.disable_ipv6=1")
+                host[pod][i][j].cmd("sysctl -w net.ipv6.conf.default.disable_ipv6=1")
+                host[pod][i][j].cmd("sysctl -w net.ipv6.conf.lo.disable_ipv6=1")
+
+    #get tablegen to initialize routing tables
+    tablegen = imp.load_source('tablegen',tablegen_path).TableGenerator(
         K=K,
         port_offset=port_offset,
         verbose=True,
         cli_path=cli_path,
         json_path=json_path
     )
-	
-	tablegen.init_all()
 
-	print "\n\n*** Topology setup done."
-	
-	if (args.test is not None):		
-		if (os.path.isfile("kickstart_python.test")):
-			print "*** Running test: {}\n\n".format(args.test)
-			CLI(net, script="kickstart_python.test")
-			print "*** Test done: {}\n\n".format(args.test)
-		else:
-			print "*** Skipping test file, it does not exist: {}\n\n".format(args.test)
-	else:
-		print "*** No test to execute."
-		# The interactive cmd will now only run if there are no tests executed.
-		print "\n*** To quit, type 'exit' or press 'Ctrl+D'."
-		CLI(net)
+    tablegen.init_all()
 
-	try:
-		net.stop()
-	except:
-		print "\n*** Quitting Mininet."
+    print "\n\n*** Topology setup done."
+
+    if (args.test is not None):
+        if (os.path.isfile("kickstart_python.test")):
+            print "*** Running test: {}\n\n".format(args.test)
+            CLI(net, script="kickstart_python.test")
+            print "*** Test done: {}\n\n".format(args.test)
+        else:
+            print "*** Skipping test file, it does not exist: {}\n\n".format(args.test)
+    else:
+        print "*** No test to execute."
+        # The interactive cmd will now only run if there are no tests executed.
+        print "\n*** To quit, type 'exit' or press 'Ctrl+D'."
+        CLI(net)
+
+    try:
+        net.stop()
+    except:
+        print "\n*** Quitting Mininet."
